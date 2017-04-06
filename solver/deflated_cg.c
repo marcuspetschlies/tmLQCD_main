dad46bc933736aebb113aeb8e9579265aa6acedf
Marcus Petschlies <marcus.petschlies@web.de>
2017-04-06 18:07:19 +0200

    deleted unused parts of code

--- solver/deflated_cg.c
+++ solver/deflated_cg.c
@@ -58,13 +58,6 @@ int exactdeflated_cg(
   static int ncurRHS=0;                  /* current number of the system being solved */                   
   static void *_ax, *_r, *_tmps1, *_tmps2;
   static spinor *ax, *r, *tmps1, *tmps2;
-#if 0
-  static _Complex double *H,*Hinv,*initwork,*tmpv1;
-  static double *hevals;
-  static int *IPIV; 
-  int tmpsize;
-  int ONE=1;
-#endif  /* of if 0 */
   static _Complex double *zhevals_inverse;
   static _Complex double *zprojected_spinor;
   static _Complex double *zprojected_spinor_weighted;
@@ -188,80 +181,6 @@ int exactdeflated_cg(
     }
 #endif
 
-#if 0
-    tmpv1 = (_Complex double *) malloc(12*N*sizeof(_Complex double));
-
-    if((evecs == NULL)  || (evals==NULL) || (tmpv1==NULL))
-    {
-       if(g_proc_id == g_stdio_proc)
-          fprintf(stderr,"[exactdeflated_cg] insufficient memory for evecs and evals inside exactdeflated_cg.\n");
-       exit(1);
-    }
-
-    H        = (_Complex double *) malloc(nconv*nconv*sizeof(_Complex double)); 
-    Hinv     = (_Complex double *) malloc(nconv*nconv*sizeof(_Complex double)); 
-    initwork = (_Complex double *) malloc(nconv*sizeof(_Complex double)); 
-    IPIV     = (int *) malloc(nconv*sizeof(int));
-    hevals      = (double *) malloc(nconv*sizeof(double));
-
-    if((H==NULL) || (Hinv==NULL) || (initwork==NULL) || (IPIV==NULL) || (hevals==NULL)) {
-       if(g_proc_id == g_stdio_proc)
-          fprintf(stderr,"[exactdeflated_cg] insufficient memory for H, Hinv, initwork, IPIV, hevals inside exactdeflated_cg.\n");
-       exit(1);
-    }
-
-    et1=gettime();
-    /* compute the elements of the hermitian matrix H 
-       leading dimension is nconv and active dimension is nconv 
-       H = V^+ Op V; V = (v_1 ... v_nconv)
-     */
-    
-    if( projection_type == 0) {
-    
-      for(i=0; i<nconv; i++)
-      {
-        assign_complex_to_spinor(r,&evecs[i*12*N],12*N);
-        f(ax,r);
-        c1 = scalar_prod(r,ax,N,parallel);
-        H[i+nconv*i] = creal(c1);  /* diagonal should be real */
-        for(j=i+1; j<nconv; j++) {
-          assign_complex_to_spinor(r,&evecs[j*12*N],12*N);
-          c1 = scalar_prod(r,ax,N,parallel);
-          H[j+nconv*i] = c1;
-          H[i+nconv*j] = conj(c1); /* enforce hermiticity */
-        }
-      }
-
-    } else if ( projection_type == 1 )  {
-
-      for(i=0; i<nconv; i++)
-      {
-        assign_complex_to_spinor(tmps1, &evecs[i*12*N], 12*N);
-        f_final(r, tmps1);
-        f(ax,r);
-        c1 = scalar_prod(r,ax,N,parallel);
-        c2 = scalar_prod(r,r,N,parallel);
-        H[i+nconv*i] = creal(c1) / creal(c2);   /* diagonal should be real */
-        for(j=i+1; j<nconv; j++)
-        {
-          assign_complex_to_spinor(tmps1, &evecs[j*12*N], 12*N);
-          f_final(r, tmps1);
-          c1 = scalar_prod(r,ax,N,parallel);
-          c3 = scalar_prod(r, r, N, parallel);
-
-          H[j+nconv*i] = c1 / sqrt( creal(c2) * creal(c3) );
-          H[i+nconv*j] = conj(c1) / sqrt( creal(c2) * creal(c3) ); /* enforce hermiticity */
-        }
-      }
-    }
-
-
-    et2=gettime();
-    if(g_proc_id == g_stdio_proc) {
-      fprintf(stdout,"[exactdeflated_cg] time to compute H: %+e\n",et2-et1);
-    }
-#endif  /* of if 0 */
-
     zhevals_inverse   = (_Complex double *) malloc(nconv*sizeof(_Complex double));
     zprojected_spinor = (_Complex double *) malloc(nconv*sizeof(_Complex double));
     zprojected_spinor_weighted = (_Complex double *) malloc(nconv*sizeof(_Complex double));
@@ -319,82 +238,6 @@ int exactdeflated_cg(
   _Complex double *zhbmv_A = NULL, *zhbmv_Y = NULL, *zhbmv_X = NULL;
 
   while( flag == -1 ) {
-#if 0
-    if(nconv > 0) {
-
-      /* ---------------------------------------------------------
-       * Perform init-CG with eigenvectors
-       * 
-       * xinit = xinit + evecs*Hinv*evec'*(b-Ax0)
-       * --------------------------------------------------------- */
-      wt1 = gettime();
-
-      /*r0=b-Ax0*/
-      f(ax,x); /*ax = A*x */
-      diff(r,b,ax,N);  /* r=b-A*x */
-
-      if( projection_type == 0) {
-
-        /* x = x + evecs*inv(H)*evecs'*r */
-        for(int i=0; i < nconv; i++) {
-           assign_complex_to_spinor(tmps1,&evecs[i*12*N],12*N);
-           initwork[i]= scalar_prod(tmps1,r,N,parallel);
-        }
-
-        /* solve the linear system H y = c */
-        tmpsize=nconv*nconv;
-        _FT(zcopy) (&tmpsize,H,&ONE,Hinv,&ONE); /* copy H into Hinv */
-        /* SUBROUTINE ZGESV( N, NRHS, A, LDA, IPIV, B, LDB, INFO ) */
-        _FT(zgesv) (&nconv,&ONE,Hinv,&nconv,IPIV,initwork,&nconv,&info_lapack);
-
-        if(info_lapack != 0) {
-           if(g_proc_id == g_stdio_proc) {
-              fprintf(stderr, "[exactdeflated_cg] Error in ZGESV:, info =  %d\n",info_lapack); 
-              fflush(stderr);
-           }
-           exit(1);
-        }
-
-        /* x = x + evecs*inv(H)*evecs'*r */
-        for(i=0; i<nconv; i++) {
-          assign_complex_to_spinor(tmps1,&evecs[i*12*N],12*N);
-          assign_add_mul(x,tmps1,initwork[i],N);
-        }
-
-      } else if ( projection_type == 1 ) {
-        /* x = x + evecs*inv(H)*evecs'*r */
-
-        /* tmps2 = Q^+ r */
-        f_initial(tmps2, r);
-
-        for(int i=0; i < nconv; i++) {
-          /* tmps1 = v_i */
-          assign_complex_to_spinor(tmps1,&evecs[i*12*N],12*N);
-
-          /* initwork_i = v_i^+ Q^+ r / lambda_i^2 */
-          initwork[i]= scalar_prod(tmps1, tmps2, N, parallel) / ( H[i*nconv+i] * H[i*nconv+i] );
-        }
-
-        memset(tmps2, 0, N*sizeof(spinor) );
-        for(i=0; i<nconv; i++) {
-          assign_complex_to_spinor(tmps1, &evecs[i*12*N], 12*N);
-          assign_add_mul(tmps2, tmps1, initwork[i], N);
-        }
-
-        /* apply final operator */
-        f_final(tmps1, tmps2);
-        assign_add_mul(x, tmps1, 1., N);
-
-      }  /* end of if projection type */
-
-      /* compute elapsed time and add to accumulator */
-
-      wt2 = gettime();
-      wI = wI + wt2-wt1;
-      
-    }  /* of if nconv > 0 */
-#endif  /* of if 0 */
-
 
     if(nconv > 0) {
 
@@ -582,14 +425,6 @@ int exactdeflated_cg(
     free(ax); free(r); free(tmps1); free(tmps2);
 #endif
 
-#if 0
-    free(H); free(Hinv);
-    free(initwork); free(tmpv1);
-    free(hevals);
-    free(IPIV);
-#endif  /* of if 0 */
-    free( evecs );
-    free( evals );
     free( zhevals_inverse );
     free( zprojected_spinor );
     free( zprojected_spinor_weighted );
